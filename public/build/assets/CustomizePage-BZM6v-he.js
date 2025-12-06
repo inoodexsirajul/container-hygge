7443bbbe264dcdccfec2669e86ddb3c95e4d1a9d
inoodexsirajul <sirajul@inoodex.com>
2025-12-06 18:02:29 +0600

    add mail feature on confirm order

--- public/build/assets/CustomizePage-BZM6v-he.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BpbsB3Iy.js";import{C as t}from"./CustomOrderBanner-BqAjw5uS.js";import"./index-DDjIHuE0.js";const m=()=>r.jsx("div",{children:r.jsx(t,{})});export{m as default};
